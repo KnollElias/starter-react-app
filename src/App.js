983ad929730288cec4b0a3f64990b5ebcf4450c1
Elias <knoll.elias@icloud.com>
2023-05-15 20:14:28 +0200

    [main] Working on NavBar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Sidebar from './components/Sidebar/Sidebar';
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const Sidebar = () => {
-  const sidebarWidth = '10%';
-  const imageWidth = '90%';
+  const sidebarWidth = '90%';
+  const imageWidth = '100%';
 
   return (
     <div style={{ backgroundColor: "white", width: sidebarWidth, height: "100vh", }}>
       <img src="./Logo-Luna-bunt.png" alt="logo" style={{ width: imageWidth }} />
-      <div>Sidebar</div>
+      <div>Cockpit</div>
+      <div>Profile</div>
+      <div>Speiseplan</div>
+      <div>Dienstplan</div>
+      <div>Pinnwand</div>
+      <div>Über Kita Luna</div>
     </div>
   );
 };
