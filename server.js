75b643a68bb6566b2b3dc22deed11c95b8d7a5c7
Elias <knoll.elias@icloud.com>
2023-05-15 17:33:03 +0200

    [main] added Typescript

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-
 const express = require('express')
 const path = require("path");
 const app = express()
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
 var options = {
   dotfiles: 'ignore',
   etag: false,
