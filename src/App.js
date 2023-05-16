897a1a0b33cf69bab4b17246cdb42ad4e9d0075b
Elias <knoll.elias@icloud.com>
2023-05-16 09:00:40 +0200

    [main] Working on NavBar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Sidebar from './components/Sidebar/Sidebar';
+
+import Login from './screens/Login/Login';
 import Cockpit from './screens/Cockpit/Cockpit';
+import Profile from './screens/Profile/Profile';
+import Speiseplan from './screens/Speiseplan/Speiseplan';
+import Dienstplan from './screens/Dienstplan/Dienstplan';
+import Pinnwand from './screens/Pinnwand/Pinnwand';
+import Ueber from './screens/Ueber/Ueber';
+// import Logout from './screens/Logout/Logout';
 
 function App() {
   return (
@@ -14,9 +22,14 @@ function App() {
         </div>
         <div className="main">
           <Routes>
+            <Route path="/login" element={<Login />} />
             <Route path="/cockpit" element={<Cockpit />} />
-            {/* Add other routes here... */}
-            {/* <Route path="/profile" element={<Profile />} /> */}
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/speiseplan" element={<Speiseplan />} />
+            <Route path="/dienstplan" element={<Dienstplan />} />
+            <Route path="/pinnwand" element={<Pinnwand />} />
+            <Route path="/ueber" element={<Ueber />} />
+            {/* <Route path="/logout" element={<Logout />} /> */}
           </Routes>
         </div>
       </div>
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 const Sidebar = () => {
+  const location = useLocation();
+
   const sidebarWidth = '90%';
   const imageWidth = '100%';
 
+  const SidebarElement = (props) => {
+    const isSelected = location.pathname === props.to;
+    return (
+      <Link to={props.to} style={{ textDecoration: 'none' }}>
+        <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "flex-start", width: "100%", height: "50px", backgroundColor: isSelected ? "red" : "white" }}>
+          <img src={props.img} alt="logo" style={{ width: "30px", height: "30px", margin: "10px" }} />
+          <div style={{ margin: "10px", color: isSelected ? "white" : "black" }}>{props.text}</div>
+        </div>
+      </Link>
+    );
+  };
+
   return (
-    <div style={{ backgroundColor: "white", width: sidebarWidth, height: "100vh", }}>
-      <img src="./Logo-Luna-bunt.png" alt="logo" style={{ width: imageWidth }} />
-      <div>Cockpit</div>
-      <div>Profile</div>
-      <div>Speiseplan</div>
-      <div>Dienstplan</div>
-      <div>Pinnwand</div>
-      <div>Über Kita Luna</div>
+    <div style={{ backgroundColor: "transparent", width: sidebarWidth, height: "100vh", marginLeft: "5%" }}>
+      <img src="./assets/Logo-Luna-bunt.png" alt="logo" style={{ width: imageWidth }} />
+      <br />
+      
+      <SidebarElement to="/cockpit" img={"./assets/sidebar/Cockpit.png"} text={"Cockpit"} />
+      <SidebarElement to="/profile" img={"./assets/sidebar/Profile.png"} text={"Profile"} />
+      <SidebarElement to="/speiseplan" img={"./assets/sidebar/Speiseplan.png"} text={"Speiseplan"} />
+      <SidebarElement to="/dienstplan" img={"./assets/sidebar/Dienstplan.png"} text={"Dienstplan"} />
+      <SidebarElement to="/pinnwand" img={"./assets/sidebar/Pinnwand.png"} text={"Pinnwand"} />
+      <SidebarElement to="/ueber" img={"./assets/sidebar/Cockpit.png"} text={"Über Kita Luna"} />
+      <SidebarElement to="/logout" img={"./assets/sidebar/Logout.png"} text={"Log Out"} />
     </div>
   );
 };
