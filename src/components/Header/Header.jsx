569f3636b4224ec5ee9b877948a575f98ada6a34
KnollElias <107244530+KnollElias@users.noreply.github.com>
2023-05-18 10:25:53 +0200

    [main] worked on Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,30 @@
 import "./Header.css"
 import React, { useState } from 'react'
 
+import Card from "./Card"
+
 const Header = () => {
   const [kidAvailability, setKidAvailability] = useState({ kids: 50, available: 20 })
   return (
-    <div>
-      <div className="availableDiv">
-        <img src="./assets/header/Av.png" 
-        alt="logo" 
-        className="image"
-        ></img>
-        Available 
-      </div>
-      <div className="notAvailableDiv">
-        <img src="./assets/header/NAv.png" 
-        alt="logo" 
-        className="image"
-        ></img>
-        Available 
-      </div>
+    <div className="row">
+     <Card 
+        mainColor={"#FF9BA6"}
+        rootText={"Kinder"}
+        dataText={"Anwesend"}
+        dataCount={20}
+        imgSrc={"./assets/header/NAv.png"} />
+     <Card 
+        mainColor={"#A59FFA"} 
+        rootText={"Kinder"}
+        dataText={"Abwesend"}
+        dataCount={30}
+        imgSrc={"./assets/header/Av.png"} />
+     <Card 
+        mainColor={"#FDB77A"} 
+        rootText={"Zusatztag"}
+        dataText={"Ausstehend"}
+        dataCount={5}
+        imgSrc={"./assets/header/NAv.png"} />
     </div>
   )
 }
