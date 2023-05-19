4d0b0e5926c08b0179928a6afdd897afcb6a914e
KnollElias <107244530+KnollElias@users.noreply.github.com>
2023-05-19 18:58:43 +0200

    [main] Worked on HEader and Cockpit(Dashboard)

--- src/screens/Cockpit/Cockpit.js
+++ src/screens/Cockpit/Cockpit.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import LocationSelector from '../../components/LocationSelector/LocationSelector'
+import EventCards from './EventCards'
+
 const Cockpit = () => {
   return (
-    <div>Cockpit</div>
+    <div>
+      <div style={{textAlign: "left", fontWeight: "bold", marginLeft: 20}}>Events Filtern</div>
+      <LocationSelector />
+      <EventCards />
+    </div>
   )
 }
 
---
+++ src/screens/Cockpit/EventCards.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import uri from "../../service/server"
+
+const EventCards = () => {
+
+  return (
+    <div>
+      EventCards
+    </div>
+  );
+}
+
+export default EventCards;
