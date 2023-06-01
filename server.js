17a1dca73f00d67e2d8d8cd06dae48a794eacaa8
Kürşat Karslı <61808010+kursatkarsli@users.noreply.github.com>
2023-06-01 19:48:10 +0300

    Ueber

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 const express = require('express')
 const path = require("path");
 const app = express()
+const cors = require('cors');
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
 // listed in the array.
+
 var options = {
   dotfiles: 'ignore',
   etag: false,
@@ -14,8 +16,8 @@ var options = {
   maxAge: '1m',
   redirect: false
 }
+app.use(cors({origin:['http://16.16.168.18:8080']}))
 app.use(express.static('build', options))
-
 const port = process.env.PORT || 4000
 
 app.listen(port, () => {
--- src/screens/Ueber/Ueber.jsx
+++ src/screens/Ueber/Ueber.jsx
@@ -1,19 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import uri from "../../service/server";
 const BASE_URL = uri.uri;
 
 const Ueber = () => {
-  return (
-    <><div>Standort Daten</div>
-      Loc 1
-
-      Loc 2
-      Loc 3
-
-      ...
-    
+  const [events, setEvents] = useState([]);
+  // const setLocation = async () => {
+  //   let request = { requestType: "About" };
+  //   try {
+  //     const response = await fetch(
+  //       `${BASE_URL}/About/${editedLocationIndex}/${tempLocationName}/${tempLocationDescription}/?requestType=${request.requestType}`,
+  //       {
+  //         method: "PATCH",
+  //       }
+  //     );
+  //     const data = await response.json();
+  //     return data;
+  //   } catch (err) {
+  //     console.warn(err);
+  //   }
+  // };
+  const getAllLocation = async () => {
+    let request = { requestType: "About" };
+    try {
+      const response = await fetch(
+        `${BASE_URL}/About?requestType=${request.requestType}`,
+        {
+          method: "GET",
+          mode: "no-cors",
+        }
+      );
+      console.log('RESPONSE', response)
+      const data = await response.json();
+      debugger
+        setEvents(data)
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+  useEffect( async () => {
+    getAllLocation().then(res=>     console.log('GET ALL ', res)
+    );
+  }, []);
+  return Object.values(events).map((event, index) => (
+    <>
+      {event.name ? (
+        <div key={event._id} className="event-card">
+          <div className="event-details">
+            <p className="event-name">{event.name}</p>
+            <p className="event-type">{event.desc}</p>
+          </div>
+        </div>
+      ) : null}
     </>
-  )
-}
+  ));
+};
 
-export default Ueber
+export default Ueber;
