53f6a6e223f19c311e5bfddef6f555e57a76fe62
Kürşat Karslı <61808010+kursatkarsli@users.noreply.github.com>
2023-06-01 20:43:08 +0300

    [main] Events added

--- src/screens/Ueber/Ueber.jsx
+++ src/screens/Ueber/Ueber.jsx
@@ -3,15 +3,18 @@ import uri from "../../service/server";
 const BASE_URL = uri.uri;
 
 const Ueber = () => {
+  const [events, setEvents] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         console.log("Making Request... ");
-        const response = await fetch('/about');
+        const response = await fetch("/about");
         console.log("Made Request... ", response);
         if (response.ok) {
           const data = await response.json();
+          console.log("RESPONSE", data);
+          setEvents(data);
         } else {
           const errorStatus = response.status;
           throw new Error(`Failed to fetch data. Status: ${errorStatus}`);
@@ -22,9 +25,26 @@ const Ueber = () => {
     };
 
     fetchData();
-  }, []);   
-
-  return <>Test</>
+  }, []);
+  console.log("DATA", events[0]);
+  return (
+    <div className="event-cards-container ">
+      {events[0] &&
+        Object.values(events[0]).map((event, index) => (
+                
+          <>
+            {event && event.name ? (
+              <div key={event._id} className="event-card">
+                <div className="event-details">
+                  <p className="event-name">{event.name}</p>
+                  <p className="event-type">{event.desc}</p>
+                </div>
+              </div>
+            ) : null}
+          </>
+        ))}
+    </div>
+  );
 };
 
 export default Ueber;
